4fe314e9e6baa8488e54f7ab39c9967254e610d9
KaremMetrial <karem.matrial@gmail.com>
2024-09-27 02:13:41 +0300

    active forget password page

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -35,7 +35,7 @@ class PasswordResetLinkController extends Controller
         $status = Password::sendResetLink(
             $request->only('email')
         );
-
+        toastr('The reset link was sent to your Email','success');
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
                     : back()->withInput($request->only('email'))
